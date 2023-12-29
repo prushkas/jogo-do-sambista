7ca0a3e45fe02462355b0543e91cd08408c5cab7
prushkas <cefasnatanael@gmail.com>
2023-12-28 22:30:33 -0300

    terminei o player e tenho q fazer o inimigo

--- cartola perdeu os instrumentos/Assets/scripts/GroundCheck.cs
+++ cartola perdeu os instrumentos/Assets/scripts/GroundCheck.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GroundCheck : MonoBehaviour
 {
     public Player player;
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.layer == 6)
         {
--- cartola perdeu os instrumentos/Assets/scripts/Player.cs
+++ cartola perdeu os instrumentos/Assets/scripts/Player.cs
@@ -8,13 +8,20 @@ public class Player : MonoBehaviour
     public float speed;
     public float jumpForce;
     public bool isJump;
+    private bool isFire;
+    public float timeToExitAttack;
+    public float movement;
 
     private Rigidbody2D rig;
+    public Transform firePoint;
+    public GameObject musicNote;
 
+    public Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,11 +29,13 @@ public class Player : MonoBehaviour
     {
         Move();
         Jump();
+        SoulFire();
+        animacione();
     }
 
     void Move()
     {
-        float movement = Input.GetAxis("Horizontal");
+        movement = Input.GetAxis("Horizontal");
 
         rig.velocity = new Vector2(movement * speed, rig.velocity.y);
 
@@ -46,10 +55,64 @@ public class Player : MonoBehaviour
         {
             if (!isJump)
             {
-                //anim.SetBool("isjump", true);
                 rig.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
                 isJump = true;
             }
         }
+    }
+
+    void SoulFire()
+    {
+        StartCoroutine("Fire");
+    }
+
+    void animacione()
+    {
+        if (isJump)
+        {
+            anim.SetInteger("transition", 2);
+        }
+        else
+        {
+            if(movement != 0)
+            {
+                anim.SetInteger("transition", 1);
+            }
+            else
+            {
+                anim.SetInteger("transition", 0);
+            }
+        }
+    }
+
+    IEnumerator Fire()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            if (isFire == false)
+            {
+                isFire = true;
+                anim.SetTrigger("attacking");
+                GameObject bolaFogo = Instantiate(musicNote, firePoint.position, firePoint.rotation);
+                Invoke(nameof(exitAttack), timeToExitAttack);
+
+                if (transform.rotation.y == 0)
+                {
+                    bolaFogo.GetComponent<Notamusical>().isRight = true;
+                }
+                if (transform.rotation.x == 180)
+                {
+                    bolaFogo.GetComponent<Notamusical>().isRight = false;
+                }
+
+                yield return new WaitForSeconds(0.3f);
+            }
+        }
+    }
+
+    void exitAttack()
+    {
+        isFire = false;
+        anim.SetBool("isfiring", false);
     }
 }
